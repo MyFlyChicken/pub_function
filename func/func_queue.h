c82c8579bd263da04b449b71d656e977cb5b4dec
禹云飞 <yuyf@audfly2020.com>
2024-06-12 14:51:40 +0800

    添加func_queue注释，最多只能存size-1个元素

--- func/func_queue.h
+++ func/func_queue.h
@@ -9,7 +9,7 @@ extern "C" {
 typedef struct {
     void*          base;         /*队列容器基地址*/
     unsigned short element_size; /*队列元素大小，即每次put元素是几个字节*/
-    unsigned short size;         /*队列容器可容纳元素个数*/
+    unsigned short size;         /*队列容器可容纳元素个数，实际缓存的元素个数为(size-1)*/
     unsigned short front;        /*队首,每次取数据时，都是在队首取出。其实是数据基于base的偏移*/
     unsigned short rear;         /*队尾,每次压入数据时，都是在队尾进行压入。其实是数据基于base的偏移*/
 } func_queue_t;
